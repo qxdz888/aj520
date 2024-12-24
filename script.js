6989b1b6c1c427e55a1f98a31568274385349c07
qxdz888 <161924047+qxdz888@users.noreply.github.com>
2024-12-24 14:58:12 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+addEventListener('DOMContentLoaded'(){
+    const gallerygetElementById('gallery')
+    const images[
+        'images/image1.jpg'
+        'images/image2.jpg'
+        'images/image3.jpg'
+        // 添加更多图片路径
+    ]
+
+forEach({
+        const ('img')
+src
+'图片展示'
+appendChild()
+    })
+})
